c5a3aaadc9a3c1c4839d7ef92828e4d5ec9f55f4
CJ <vssrcj@gmail.com>
2019-10-15 08:12:08 +0200

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `CJ`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,13 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `rw58xev7q34a`,
+        accessToken: `GdihvLc1BLtpwcQGUgisHvIQ-8xnxLf2SPPpyPvMuAg`
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
